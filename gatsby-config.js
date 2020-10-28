d19cef7b9a8380452c86a491e7c46fff716e681d
Nikolas Heidner <niko.heidner@gmail.com>
2020-10-28 18:31:42 +0000

    delet asset entry in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,14 +16,6 @@ module.exports = {
             },
         },
         {
-            // keep as first gatsby-source-filesystem plugin for gatsby image support
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                path: `${__dirname}/src/assets`,
-                name: 'assets',
-            },
-        },
-        {
             resolve: 'gatsby-source-filesystem',
             options: {
                 path: `${__dirname}/src/markdown`,
